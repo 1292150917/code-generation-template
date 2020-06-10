4bdbf45e22f7dd89096a712c91124947dfe91fd7
zhangzifang <1292150917@qq.com>
2020-06-10 22:48:36 +0800

    ..

--- src/views/database.vue
+++ src/views/database.vue
@@ -1,25 +1,27 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-06-04 18:31:33
+ * @LastEditTime: 2020-06-10 22:03:56
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \nodec:\Users\zhamgzifang\Desktop\code-generation-template\src\views\database.vue
+--> 
 <template>
   <div class="database">
     <div class="database-left">
-      <div v-for="(item,index) in 10" :key="index">
+      <div
+        v-for="(item,index) in surfaceList"
+        :class="tagIndex === index ? 'f1f1f1' : ''"
+        @click="tagClick(item,index)"
+        :key="index"
+      >
         <img src="../assets/table.png" alt />
-        {{item}}
+        {{item.name}}
       </div>
     </div>
     <div class="database-right">
       <el-table :data="tableData" border style="width: 100%">
-        <el-table-column fixed prop="date" label="日期" width="150"></el-table-column>
-        <el-table-column prop="name" label="姓名" width="120"></el-table-column>
-        <el-table-column prop="province" label="省份" width="120"></el-table-column>
-        <el-table-column prop="city" label="市区" width="120"></el-table-column>
-        <el-table-column prop="address" label="地址"></el-table-column>
-        <el-table-column prop="zip" label="邮编"></el-table-column>
-        <el-table-column fixed="right" label="操作">
-          <template slot-scope="scope">
-            <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
-            <el-button type="text" size="small">编辑</el-button>
-          </template>
-        </el-table-column>
+        <el-table-column :prop="item.Field" :key="index" v-for="(item,index) in tableColumn" :label="item.Field"></el-table-column>
       </el-table>
     </div>
   </div>
@@ -30,53 +32,56 @@ export default {
   name: "App",
   data() {
     return {
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1518 弄",
-          zip: 200333
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1517 弄",
-          zip: 200333
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1519 弄",
-          zip: 200333
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1516 弄",
-          zip: 200333
-        }
-      ]
+      surfaceList: [],
+      tables: {},
+      tableDataKey: [],
+      tagIndex: "",
+      tableColumn:[],
+      tableData: []
     };
   },
+  watch: {
+    tagIndex(res) {
+      var name = this.surfaceList[res].name
+      this.tableColumn = this.tables[name]
+    }
+  },
   methods: {
+    async tablesSurface() {
+      var list = await this.$http({ url: "tables/surface", data: {} });
+      this.surfaceList = list.data;
+      if (list.data) {
+        this.listPage(list.data[0].name);
+      }
+    },
+    async listPage(name) {
+      var listPage = await this.$http({
+        url: "tables/listPage",
+        data: { name }
+      });
+      this.tableData = listPage.data
+    },
+    tagClick(_, index) {
+      this.tagIndex = index;
+    },
     handleClick(row) {
       console.log(row);
     }
   },
-  async created() {}
+  async created() {
+    var tables = await this.$http({ url: "tables", data: {} });
+    this.tables = tables.data;
+    await this.tablesSurface();
+    this.tagIndex = 0;
+  }
 };
 </script>
 <style lang="scss" scoped="scoped">
-.database{
-    height: 100%;
+.database {
+  height: 100%;
+}
+.f1f1f1 {
+  background: #f1f1f1;
 }
 .database-left {
   line-height: 40px;
